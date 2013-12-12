6f12b72a6411b996b768d8741f56ec2dbf93ae83
Masaki Waga <masakiwaga+robotech@gmail.com>
2013-12-12 16:55:55 +0900

    patch for IS2014 applyed

--- cancel.sh
+++ cancel.sh
@@ -19,6 +19,8 @@ for key in $(cat `dirname $0`/search.list) ; do
         if [ $(grep -c "$line2" `dirname $0`/cancel.cache) -eq 0 ] && [[ ! $line2 =~ ^$ ]]; then
 	        echo "$line2" >> `dirname $0`/cancel.cache;
 	        name=$(echo $line2 | awk -F "," '{print $4 "休講" }' )
+ 	        name2=$(echo $line2 | awk -F "," '{print "明日の" $4 "は休講です." }' )
+ 	        name3=$(echo $line2 | awk -F "," '{print "今日の" $4 "は休講です." }' )
 	        place=$(echo $line2 |awk -F "," '{print $6 }' )
 	        d=$(echo $line2 | awk -F "," '{print $1 }' )
 	        class=$(echo $line2 | awk -F "," '{print $3 }' )
@@ -30,6 +32,8 @@ for key in $(cat `dirname $0`/search.list) ; do
 	            year=$((`date +"%Y"` + 1));
 	        fi
 #	        ruby `dirname $0`/add_schedule.rb $name $place $year $month $date $class
+#               ruby `dirname $0`/add_schedule.rb $name2 $place $year $month $(($date - 1)) 3
+# 	        ruby `dirname $0`/add_schedule.rb $name3 $place $year $month $date 1 -10
 	        if $VERBOSE ; then echo $name $place $year $month $date $class ; fi
         fi
     done
