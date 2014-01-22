0cc051e26ab6c715f8671573628912ed6d6ff306
Masaki Waga <masakiwaga+robotech@gmail.com>
2014-01-22 16:10:55 +0900

    理学部情報科学科の休講情報のページ(http://www.is.s.u-tokyo.ac.jp/no-lec.php)に対応した.というかそっち用に修正した.

--- cancel.sh
+++ cancel.sh
@@ -4,24 +4,20 @@ SED=sed
 
 touch `dirname $0`/cancel.cache
 
-if [ $# -gt 0 ] && [ $1 == '-v' ] ; then VERBOSE=( exit 1 );else VERBOSE=( exit 0);fi
+#if [ $# -gt 0 ] && [ $1 == '-v' ] ; then VERBOSE=( exit 1 );else VERBOSE=( exit 0);fi
 
-curl -s http://www.c.u-tokyo.ac.jp/zenki/classes/cancel/index.html | grep '</\?t[rd].*' | tr -d ',\t\n' | $SED -e 's:/tr>:/tr>\n:g'| $SED -e 's/<[^>]*>/,/g' | $SED -e 's/[[:blank:]]//g;s/,,*/,/g'| $SED -e 's/^,//g;s/,$//g' | awk -F "," '{
-if( NF == 4) for ( t = 3 ; t >=1 ; t-- ) { $(t+3)=$t; $t = L[t];}
-else for ( t = 3 ; t >=1 ; t-- ) L[t]=$t;
-print;
-}' | tr ' ' ',' > `dirname $0`/tmp.csv
+curl -s http://www.is.s.u-tokyo.ac.jp/no-lec.php | awk 'BEGIN{a=0}/<\/tbody>/{exit}a&&/<\/?t/{print}/<tbody>/{a=1}' | tr '\n' ' ' | $SED -e 's/[[:blank:]]//g' | $SED -e 's/<tr><td>//g;s:</td><td>:,:g;s:</td></tr>:\n:g' | nkf  > `dirname $0`/tmp.csv
 
 for key in $(cat `dirname $0`/search.list) ; do
-    if $VERBOSE ; then echo $key;fi
+    echo $key;#fi
     line=$(grep "$key" `dirname $0`/tmp.csv)
     for line2 in $line ; do
         if [ $(grep -c "$line2" `dirname $0`/cancel.cache) -eq 0 ] && [[ ! $line2 =~ ^$ ]]; then
 	        echo "$line2" >> `dirname $0`/cancel.cache;
-	        name=$(echo $line2 | awk -F "," '{print $4 "休講" }' )
-	        place=$(echo $line2 |awk -F "," '{print $6 }' )
+	        name=$(echo $line2 | awk -F "," '{print $3 "休講" }' )
+	        place=$(echo $line2 |awk -F "," '{print $5 }' )
 	        d=$(echo $line2 | awk -F "," '{print $1 }' )
-	        class=$(echo $line2 | awk -F "," '{print $3 }' )
+	        class=${d#*)};
 	        month=$(echo $d | sed -e 's:月.*$::g')
 	        date=$(echo $d | sed -e 's:日.*$::g;s:^.*月::g')
 	        if [ $month -ge `date +"%m"` ] ; then 
@@ -30,7 +26,7 @@ for key in $(cat `dirname $0`/search.list) ; do
 	            year=$((`date +"%Y"` + 1));
 	        fi
 #	        ruby `dirname $0`/add_schedule.rb $name $place $year $month $date $class
-	        if $VERBOSE ; then echo $name $place $year $month $date $class ; fi
+	        echo $name $place $year $month $date $class ;
         fi
     done
 done
