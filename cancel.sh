2a45b0324b1e8bf3cd94181f1107ec4bf3ac81c3
Masaki Waga <masakiwaga@gmail.com>
2014-03-02 16:57:57 +0900

    modified to use awk in main loop

--- cancel.sh
+++ cancel.sh
@@ -4,33 +4,58 @@ SED=sed
 
 touch `dirname $0`/cancel.cache
 
-if [ $# -gt 0 ] && [ $1 == '-v' ] ; then VERBOSE=( exit 1 );else VERBOSE=( exit 0);fi
+#if [ $# -gt 0 ] && [ $1 == '-v' ] ; then VERBOSE=( exit 1 );else VERBOSE=( exit 0);fi
 
-curl -s http://www.c.u-tokyo.ac.jp/zenki/classes/cancel/index.html | grep '</\?t[rd].*' | tr -d ',\t\n' | $SED -e 's:/tr>:/tr>\n:g'| $SED -e 's/<[^>]*>/,/g' | $SED -e 's/[[:blank:]]//g;s/,,*/,/g'| $SED -e 's/^,//g;s/,$//g' | awk -F "," '{
+curl -s http://www.c.u-tokyo.ac.jp/zenki/classes/cancel/index.html | grep '</\?t[rd].*' | tr -d ',\t\n' | $SED -e 's:/tr>:/tr>\n:g'| $SED -e 's/<[^>]*>/,/g' | $SED -e 's/[[:blank:]]//g;s/,,*/,/g'| $SED -e 's/^,//g;s/,$//g' | gawk -F "," '{
 if( NF == 4) for ( t = 3 ; t >=1 ; t-- ) { $(t+3)=$t; $t = L[t];}
 else for ( t = 3 ; t >=1 ; t-- ) L[t]=$t;
 print;
 }' | tr ' ' ',' > `dirname $0`/tmp.csv
 
-for key in $(cat `dirname $0`/search.list) ; do
-    if $VERBOSE ; then echo $key;fi
-    line=$(grep "$key" `dirname $0`/tmp.csv)
-    for line2 in $line ; do
-        if [ $(grep -c "$line2" `dirname $0`/cancel.cache) -eq 0 ] && [[ ! $line2 =~ ^$ ]]; then
-	        echo "$line2" >> `dirname $0`/cancel.cache;
-	        name=$(echo $line2 | awk -F "," '{print $4 "休講" }' )
-	        place=$(echo $line2 |awk -F "," '{print $6 }' )
-	        d=$(echo $line2 | awk -F "," '{print $1 }' )
-	        class=$(echo $line2 | awk -F "," '{print $3 }' )
-	        month=$(echo $d | sed -e 's:月.*$::g')
-	        date=$(echo $d | sed -e 's:日.*$::g;s:^.*月::g')
-	        if [ $month -ge `date +"%m"` ] ; then 
-	            year=`date +"%Y"`;
+gawk -F ',' -v verbose=$VERBOSE -v keyfile=`dirname $0`/search.list -v cachefile=`dirname $0`/cancel.cache -v m=`date +"%m"` -v y=`date +"%Y"` '
+BEGIN {
+	numOfCache=0;
+	while ( (getline var < cachefile) > 0) {
+               cache[numOfCache++] = var
+        }
+        close(cachefile)
+
+	numOfKeys=0;
+	while ( (getline var < keyfile) > 0) {
+               keys[numOfKeys++] = var
+        }
+        close(keyfile)
+}
+
+function matchOr ( key , array, num) {
+	for ( t = 0; t < num ; t++)  {
+		if ( key ~ array[t] ) {
+			return 1;
+		}
+	}
+	return 0;
+}
+
+NF > 0 && matchOr( $0 , keys , numOfKeys){
+	if ( verbose ) 
+		print;
+	if ( system("[ $(grep -c \"" $0 "\" `dirname $0`/cancel.cache) -gt 0 ] " ) ){
+		print >> cachefile;
+	        name = $4 "休講";
+	        place = $6;
+	        class = $3;
+	        month = $1;
+		sub(/月.*$/,"",month)
+	        date = $1;
+		sub(/日.*$/,"",date)
+		sub(/^.*月/,"",date)
+	        if ( month >= m ) 
+	            year = y
 	        else
-	            year=$((`date +"%Y"` + 1));
-	        fi
-#	        ruby `dirname $0`/add_schedule.rb $name $place $year $month $date $class
-	        if $VERBOSE ; then echo $name $place $year $month $date $class ; fi
-        fi
-    done
-done
+	            year = y+1
+#	        system("ruby `dirname $0`/add_schedule.rb" name place year month date class)
+	        if (verbose) 
+			print name place year month date class;
+	}
+}' tmp.csv 
+
